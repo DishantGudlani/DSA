fbc2d5ad1dcbf2cb95038a0571a7d33bef223c27
gizmo <a@x.com>
2020-06-03 14:03:14 +0530

    added april leetcode

---
+++ AprilLeetCode/1.SingleNumber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(result.contains(nums[i]))
+            {
+                result.remove(new Integer(nums[i]));
+            }
+            
+            else
+            {
+                result.add(nums[i]);
+            }
+        }
+        
+        return result.get(0);
+    }
+}
